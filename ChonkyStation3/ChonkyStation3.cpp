a7a3f37f9cdf0e3ec8cd1ce2db709d654e358921
moonpower <Naalfdl@gmail.com>
2025-03-12 00:58:29 +0300

    Implement Xbox build support with external_main function and update file handling

--- ChonkyStation3/ChonkyStation3.cpp
+++ ChonkyStation3/ChonkyStation3.cpp
@@ -1,22 +1,80 @@
 #include <iostream>
+#include <filesystem>
+#include <SDL.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__((visibility("default")))
+#endif
 
 #include <Frontend/GameWindow.hpp>
 #ifdef CHONKYSTATION3_QT_BUILD
 #include <Frontend/MainWindow.hpp>
 #else
-#include "PlayStation3.hpp" // Don't need this here in Qt builds because it's included in MainWindow.hpp
+#include "PlayStation3.hpp" // Not needed in Qt builds because it's included in MainWindow.hpp
 #endif
 
-#ifdef _WIN32
-#include <windows.h>
-// Gently ask to use the discrete Nvidia/AMD GPU if possible instead of
-// integrated graphics
-extern "C" {
-__declspec(dllexport) DWORD NvOptimusEnablement = 1;
-__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 1;
+#ifdef __XBOX_BUILD
+extern "C" EXPORT int external_main(SDL_Window* host_window, SDL_GLContext host_context, int argc, char** argv) {
+    try {
+        printf("ChonkyStation3 external_main: Started\n");
+        
+        // Make the GL context current on this thread.
+        if (SDL_GL_MakeCurrent(host_window, host_context) != 0) {
+            printf("external_main: SDL_GL_MakeCurrent failed: %s\n", SDL_GetError());
+            return -1;
+        }
+        
+        if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(SDL_GL_GetProcAddress))) {
+            Helpers::panic("OpenGL init failed");
+        }
+        printf("external_main: GL context is now current\n");
+
+        // Process file path if provided will probably never be used again in UWP.
+        std::filesystem::path file = "";
+        if (argc >= 2) {
+            file = argv[1];
+            printf("external_main: File provided: %s\n", file.generic_string().c_str());
+        } else {
+            printf("external_main: No file provided, calling gameSelector\n");
+        }
+
+        PlayStation3* ps3 = new PlayStation3(file);
+        if (file.empty()) {
+            printf("external_main: Calling gameSelector()\n");
+            ps3->gameSelector();
+        }
+        printf("external_main: Initializing PS3\n");
+        ps3->init();
+
+        // Validate the host window.
+        if (!host_window) {
+            printf("external_main: Error: Provided SDL window pointer is null.\n");
+            return -1;
+        }
+        printf("external_main: Using provided SDL window: %p\n", host_window);
+
+        GameWindow game_window(host_window);
+        printf("external_main: GameWindow constructed\n");
+
+        printf("external_main: Calling game_window.run()\n");
+        game_window.run(ps3);
+        printf("external_main: Finished run()\n");
+
+        return 0;
+    } catch (const std::exception& e) {
+        fprintf(stderr, "Exception caught in external_main: %s\n", e.what());
+        return -1;
+    } catch (...) {
+        fprintf(stderr, "Unknown exception caught in external_main.\n");
+        return -1;
+    }
 }
-#endif
-
+#else
 
 int main(int argc, char** argv) {
     printf("ChonkyStation3\n\n");
@@ -30,19 +88,19 @@ int main(int argc, char** argv) {
     main_window.onExit();
     return ret;
 #else
-
-    fs::path file = "";
+    std::filesystem::path file = "";
     if (argc >= 2)
         file = argv[1];
 
     PlayStation3* ps3 = new PlayStation3(file);
     if (argc == 1)
-        ps3->gameSelector();    // Show a game selector dialog to choose one of the installed games if no direct elf path is passed via CLI
+        ps3->gameSelector(); // Launch game selector if no file path is provided.
     ps3->init();
 
-    GameWindow game_window = GameWindow();
+    GameWindow game_window;
     game_window.run(ps3);
 
     return 0;
 #endif
 }
+#endif
